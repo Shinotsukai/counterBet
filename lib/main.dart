72f93b156b64ec2b25d9bb5f07b1a5e82ebe3a62
Shinotsukai <imagi.soft@gmail.com>
2020-07-08 00:15:27 +0100

    title update

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'counterBet',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
